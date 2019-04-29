a5222c31340858653b6f279e6ef2f4461483bf60
Michal Majcherski <michal.majcherski@gft.com>
2019-04-29 17:10:50 +0200

    Windows - failing a test

--- test/HelloLibTest.scala
+++ test/HelloLibTest.scala
@@ -22,12 +22,12 @@ object TestUtil {
 
 class ScalaSuite extends FlatSpec {
   "HelloLib" should "call scala" in {
-    assert(HelloLib.getOtherLibMessage("hello").equals("hello you all, everybody. I am Runtime"))
+    assert(HelloLib.getOtherLibMessage("hello").equals("bye you all, everybody. I am Runtime"))
   }
 }
 
 class JavaSuite extends FlatSpec {
   "HelloLib" should "call java" in {
-    assert(HelloLib.getOtherJavaLibMessage("hello").equals("hello java!"))
+    assert(HelloLib.getOtherJavaLibMessage("hello").equals("bye java!"))
   }
 }
